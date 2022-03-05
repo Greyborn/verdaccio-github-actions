8e5ac5666973d3909ff31487e6fc07cbcb37cbca
somethingSTRANGE <somethingSTRANGE@users.noreply.github.com>
2022-03-05 00:22:39 -0800

    feat: replace hard values with input env variables

--- publish/entrypoint.sh
+++ publish/entrypoint.sh
@@ -2,17 +2,25 @@
 
 set -e
 
-local_registry="http://0.0.0.0:4873"
-
-# start local registry
+echo
+echo "🟢 START LOCAL REGISTRY"
 tmp_registry_log=`mktemp`
 sh -c "mkdir -p $HOME/.config/verdaccio"
 sh -c "cp --verbose /config.yaml $HOME/.config/verdaccio/config.yaml"
 sh -c "nohup verdaccio --config $HOME/.config/verdaccio/config.yaml &>$tmp_registry_log &"
-# wait for `verdaccio` to boot
-# FIXME: this throws a syntax error, but would be great to make it run
-# grep -q 'http address' <(tail -f $tmp_registry_log)
-# login so we can publish packages
-sh -c "npm-auth-to-token -u test -p test -e test@test.com -r $local_registry"
-# Run nmp command
-sh -c "npm publish --registry $local_registry $1"
+
+#echo
+#echo "💤 WAIT FOR VERDACCIO TO BOOT"
+## FIXME: this throws a syntax error, but would be great to make it run
+#grep -q 'http address' <(tail -f $tmp_registry_log)
+
+echo
+echo "🔑 LOGIN TO REMOTE REGISTRY"
+sh -c "npm-auth-to-token -u $INPUT_USERNAME -p $INPUT_PASSWORD -e $INPUT_EMAIL -r $INPUT_REGISTRY"
+
+echo
+echo "📡 PUBLISH PACKAGE TO REMOTE REGISTRY"
+sh -c "npm publish --registry $INPUT_REGISTRY $1"
+
+echo
+echo "🏁 DONE"
